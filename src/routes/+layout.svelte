38614f50aa7b056a1238e8a0cf6f1e860bbe79cc
Chamseddine Daaji <d3j1x@yahoo.com>
2025-07-09 20:10:38 +0200

    SMRT MRKT X

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,6 @@
 <script lang="ts">
-	import '../app.css';
-
-	import Header from '$lib/components/layoutComponents/Header.svelte';
 	let { children } = $props();
-	import Footer from '$lib/components/layoutComponents/Footer.svelte';
 </script>
 
-<svelte:head>
-	<script src="https://kit.fontawesome.com/bb271890d7.js" crossorigin="anonymous"></script>
-	<script
-		async
-		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5113481215151349"
-		crossorigin="anonymous"
-	></script>
-</svelte:head>
-
-<Header />
-<div class="mt-10"></div>
 {@render children()}
 
-<Footer />
