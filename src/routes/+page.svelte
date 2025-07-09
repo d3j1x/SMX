cd28910816882f51f114a5a608038b212e3e42f0
Chamseddine Daaji <d3j1x@yahoo.com>
2025-07-09 20:08:23 +0200

    refer to SMRT MRKT X - Store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,14 @@
 <script>
-	import Hero from '$lib/components/homeComponents/Hero.svelte';
-	import Poweredby from '$lib/components/homeComponents/Poweredby.svelte';
-	import HowWeWork from '$lib/components/homeComponents/HowWeWork.svelte';
-	import About from '$lib/components/homeComponents/About.svelte';
-	import Services from '$lib/components/homeComponents/Services.svelte';
+	if (typeof window !== 'undefined') {
+		window.location.href = 'https://smrt-mrkt-x.tiktak-store.com/';
+	}
 </script>
 
 <svelte:head>
 	<title>SMRT MRKT X</title>
 	<meta
 		name="description"
-		content="SMX - SMRT MRKT X - OFFICIAL WEBSITE © - We create amazing open-source projects that empower developers and foster collaboration."
+		content="SMX - SMRT MRKT X - OFFICIAL WEBSITE © - Special Edition."
 	/>
 </svelte:head>
 
-<Hero />
-
-<Services />
-
-<About />
-
-<HowWeWork />
-
-<Poweredby />
